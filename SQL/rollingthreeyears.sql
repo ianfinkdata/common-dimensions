524447d89103329b3bc6c41df8dc56ca24126cc2
Ian Fink <247514964+ianfinkdata@users.noreply.github.com>
2026-04-14 20:55:37 -0500

    dynamic calendar view creation

--- SQL/rollingthreeyears.sql
+++ SQL/rollingthreeyears.sql
@@ -1,11 +1,10 @@
-with rollingthreeyears as (
+create or replace view dynamiccalendar as
+-- Rolling three year calendar is the default 
+
 select * 
 from dim_date as cal
 where 
 year(cal.date) 
     between  year(CURRENT_DATE) - 3 
     and  year(CURRENT_DATE)
-    
-)
-
-select * from rollingthreeyears as rty;
+;
